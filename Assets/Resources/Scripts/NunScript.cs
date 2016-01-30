4c3bfd7434407f8082e802dd99ff3a268ab794af
unknown <v.birembaut@J001-0133-07.etudiant.rubika-edu.com.local>
2016-01-30 10:18:51 +0100

    Prefab

--- Assets/Resources/Scripts/NunScript.cs
+++ Assets/Resources/Scripts/NunScript.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class NunScript : MonoBehaviour {
 
 
+    int twerkMax;
+    int twerkCurrent;
+
+    bool ending = false;
+    GameObject confessionalForEnd;
+
     NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
@@ -12,14 +18,34 @@ public class NunScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if(confessionalForEnd)
+        {
+            if (Vector3.Distance(confessionalForEnd.transform.position, transform.position) < 2)
+            {
+                //lvlUpPoint
+            }
+        }
 	}
 
     public void goToTheBench(GameObject bench)
     {
-        Debug.Log(bench.transform.position);
         if(!agent)
             agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(new Vector3(bench.transform.position.x, transform.position.y, bench.transform.position.z));
+    }
+
+    public bool plzTwkMe(GameObject player)
+    {
+        twerkCurrent++;
+        if(twerkCurrent == twerkMax)
+        {
+            //confessionalForEnd = player.GetComponent<ScriptPlayer>().confessional;
+            agent.SetDestination(confessionalForEnd.transform.position);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
